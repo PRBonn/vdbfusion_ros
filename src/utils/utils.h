b3394d9cd0309ecd7dce3f4c26e99f5e921d358f
Ignacio Vizzo <ignaciovizzo@gmail.com>
2022-06-03 16:05:24 +0200

    remove unused funciton

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -3,12 +3,6 @@
 
 #include <eigen3/Eigen/Core>
 
-void InvertTransform(geometry_msgs::Transform& tf) {
-    tf::Transform transform;
-    tf::transformMsgToTF(tf, transform);
-    tf::transformTFToMsg(transform.inverse(), tf);
-}
-
 void PreProcessCloud(std::vector<Eigen::Vector3d>& points, float min_range, float max_range) {
     points.erase(
         std::remove_if(points.begin(), points.end(), [&](auto p) { return p.norm() > max_range; }),
