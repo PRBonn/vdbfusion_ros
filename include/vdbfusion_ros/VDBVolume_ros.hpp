f60dcea7c511fb9e92e4793eaba5a34a075e181a
Saurabh Gupta <saurabh.gupta1002@gmail.com>
2022-06-09 13:40:59 +0200

    Add function declaration in header file

--- include/vdbfusion_ros/VDBVolume_ros.hpp
+++ include/vdbfusion_ros/VDBVolume_ros.hpp
@@ -3,9 +3,18 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 
+#include <Eigen/Core>
+#include <vector>
+
 #include "Transform.hpp"
 #include "vdbfusion/VDBVolume.h"
 #include "vdbfusion_ros/save_vdb_volume.h"
+
+namespace {
+void pcl2SensorMsgToEigen(const sensor_msgs::PointCloud2& pcl2,
+                          std::vector<Eigen::Vector3d>& points);
+void PreProcessCloud(std::vector<Eigen::Vector3d>& points, float min_range, float max_range);
+}  // namespace
 
 namespace vdbfusion {
 class VDBVolumeNode {
